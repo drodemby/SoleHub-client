f96fa7748d8183b39a4863f0396ed8b6a57c3687
Dro Demby <drotechdemby@gmail.com>
2023-08-26 15:07:59 -0500

    Finished product form

--- pages/users/edit/[id].js
+++ pages/users/edit/[id].js
@@ -1,20 +1,20 @@
 import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
+// import { useRouter } from 'next/router';
 import Head from 'next/head';
 import { getSingleUser } from '../../../utils/data/userData';
 import RegisterForm from '../../../components/RegisterForm';
 import { useAuth } from '../../../utils/context/authContext';
 
 export default function EditUser() {
-  const router = useRouter();
-  const { id } = router.query;
+  // const router = useRouter();
+  // const { id } = router.query;
   const [editUser, setEditUser] = useState({});
   const { user } = useAuth();
   useEffect(() => {
     getSingleUser(user.uid).then((obj) => {
       setEditUser(obj);
     });
-  }, [id]);
+  }, []);
   return (
     <>
       <Head>
