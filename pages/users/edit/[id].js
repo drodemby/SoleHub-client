40000c148a86de544f4adadcf25dd8503e316484
Dro Demby <drotechdemby@gmail.com>
2023-08-26 15:08:35 -0500

    Finished product form

--- pages/users/edit/[id].js
+++ pages/users/edit/[id].js
@@ -1,28 +1,28 @@
-import React, { useEffect, useState } from 'react';
-// import { useRouter } from 'next/router';
-import Head from 'next/head';
-import { getSingleUser } from '../../../utils/data/userData';
-import RegisterForm from '../../../components/RegisterForm';
-import { useAuth } from '../../../utils/context/authContext';
+// import React, { useEffect, useState } from 'react';
+// // import { useRouter } from 'next/router';
+// import Head from 'next/head';
+// import { getSingleUser } from '../../../utils/data/userData';
+// import RegisterForm from '../../../components/RegisterForm';
+// import { useAuth } from '../../../utils/context/authContext';
 
-export default function EditUser() {
-  // const router = useRouter();
-  // const { id } = router.query;
-  const [editUser, setEditUser] = useState({});
-  const { user } = useAuth();
-  useEffect(() => {
-    getSingleUser(user.uid).then((obj) => {
-      setEditUser(obj);
-    });
-  }, []);
-  return (
-    <>
-      <Head>
-        <title>Edit User</title>
-      </Head>
-      <div>
-        <RegisterForm obj={editUser} />
-      </div>
-    </>
-  );
-}
+// export default function EditUser() {
+//   // const router = useRouter();
+//   // const { id } = router.query;
+//   const [editUser, setEditUser] = useState({});
+//   const { user } = useAuth();
+//   useEffect(() => {
+//     //getSingleUser(user.uid).then((obj) => {
+//       setEditUser(obj);
+//     });
+//   }, []);
+//   return (
+//     <>
+//       <Head>
+//         <title>Edit User</title>
+//       </Head>
+//       <div>
+//         <RegisterForm obj={editUser} />
+//       </div>
+//     </>
+//   );
+// }
