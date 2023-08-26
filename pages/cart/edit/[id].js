9fe97fcf2994a3e25afbd47be9db14eb8a3d26f7
Dro Demby <drotechdemby@gmail.com>
2023-08-26 13:06:51 -0500

    clean

--- pages/cart/edit/[id].js
+++ pages/cart/edit/[id].js
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from 'react';
 
 function EditCart() {
   return (
     <div>EditCart</div>
-  )
+  );
 }
 
-export default EditCart
+export default EditCart;
